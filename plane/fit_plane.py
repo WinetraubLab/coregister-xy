e77b3d7769890b4f1b7122b1809cc8f645a8041f
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2024-07-02 14:24:07 -0700

    Create unit test for translation

--- plane/fit_plane.py
+++ plane/fit_plane.py
@@ -4,7 +4,8 @@ import numpy as np
 class FitPlane:
     
     """ Begin constractor methods """
-    def __init__():
+    def __init__(self, M=None):
+        self.M = M
  
     
     @classmethod
@@ -17,3 +18,41 @@ class FitPlane:
             source_image_points: pixel location of points of interest as an array or numpy array [[x0,y0],[x1,y1],...]
             dest_image_points: pixel location of points of interest as an array or numpy array [[x0,y0],[x1,y1],...]
         """
+        # Type conversion
+
+        source_image_points = np.array(source_image_points, dtype=np.float32)
+        dest_image_points = np.array(dest_image_points, dtype=np.float32)
+        assert source_image_points.shape == dest_image_points.shape, "Number of points must match"
+
+        # Create least squared matrix equations for quadratic transformation
+        A = []
+        B = []
+        for i in range(6):
+            x_dest, y_dest = dest_image_points[i]
+            x, y = source_image_points[i]
+            A.append([x**2,y**2,x*y,x,y,1])
+            B.append([x_dest, y_dest])
+        B = np.array(B)
+        A = np.array(A, dtype=np.float32)
+        assert B.shape == (6,2), "Shape of matrix B incorrect"
+        assert A.shape == (6,6), "Shape of matrix A incorrect"
+
+        # Solve least squared equation
+        M = np.linalg.solve(A,B)
+        return cls(M)
+
+    
+    def transform_point(self, source_point):
+        """
+        This function transforms a source point to destination point.
+        Inputs: 
+            source_points: pixel locations of a point as an array [x,y]
+        """
+        M = self.M
+        x,y = source_point
+        x_new = M[0,0]*x**2 + M[1,0]*y**2 + M[2,0]*x*y + M[3,0]*x + M[4,0]*y + M[5,0]
+        y_new = M[0,1]*x**2 + M[1,1]*y**2 + M[2,1]*x*y + M[3,1]*x + M[4,1]*y + M[5,1]
+
+        return np.array([x_new, y_new])
+    
+
--- plane/test_fit_plane.py
+++ plane/test_fit_plane.py
@@ -2,26 +2,26 @@ import math
 import numpy as np
 import numpy.testing as npt
 import unittest
+from fit_plane import FitPlane
 
 class TestFitPlane(unittest.TestCase):
 
     # Define default pattern
     def setUp(self):
-      self.source_image_points = np.array([ [20,60], [20, 10], [60, 10], [30, 20], [30, 45], [45, 45])
-            
-    def assertAlmostEqualRelative(self, first, second, rel_tol=1e-3, msg=None):
-        if not math.isclose(first, second, rel_tol=rel_tol):
-            standard_msg = f'{first} != {second} within {rel_tol} relative tolerance'
-            self.fail(self._formatMessage(msg, standard_msg))
-                
+      self.source_image_points = np.array([ [20,60], [20, 10], [60, 10], [30, 20], [30, 45], [45, 45]])
+
     def test_main_function_runs(self):
-      raise NotImplemented()
+      FitPlane.from_fitting_points_between_fluorescence_image_and_template(self.source_image_points, self.source_image_points)
 
     def test_translation(self):
-      dest_image_points = np.array([[p[0]+10,p[1]+10] for p in self.source_image_points.toArray()])
-      fp = 1  # TBD
+      dest_image_points = np.array([[p[0]+10,p[1]+5] for p in self.source_image_points])
+
+      fp = FitPlane.from_fitting_points_between_fluorescence_image_and_template(self.source_image_points, dest_image_points)
+      result = fp.transform_point([0,0])
+      self.assertAlmostEqual(result[0], 10)
+      self.assertAlmostEqual(result[1], 5)
 
       # Apply the transformation on the source points and make sure that it matches destination points
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
