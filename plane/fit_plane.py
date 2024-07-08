2db6335d15fd041b9e9ee83ed347afc76e4699d5
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2024-07-08 11:15:39 -0700

    affine/quadratic option

--- plane/fit_plane.py
+++ plane/fit_plane.py
@@ -5,40 +5,49 @@ import cv2 as cv
 class FitPlane:
     
     """ Begin constractor methods """
-    def __init__(self, M=None, M_rev=None):
+    def __init__(self, M=None, M_rev=None, order=None):
         self.M = M # Transformation from source image to dest image coordinates
         self.M_rev = M_rev # Reverse transformation
+        self.order = order
     
     @classmethod
     def from_fitting_points_between_fluorescence_image_and_template(cls, 
-        source_image_points, dest_image_points):
+        source_image_points, dest_image_points, order=1):
         """
         This function initialize FitPlane by points on both source and destination images.
         
         INPUTS:
             source_image_points: pixel location of points of interest as an array or numpy array [[x0,y0],[x1,y1],...]
             dest_image_points: pixel location of points of interest as an array or numpy array [[x0,y0],[x1,y1],...]
+            order: 1 for affine, 2 for quadratic.
         """
         # Type conversion
 
         source_image_points = np.array(source_image_points, dtype=np.float32)
         dest_image_points = np.array(dest_image_points, dtype=np.float32)
         assert source_image_points.shape == dest_image_points.shape, "Number of points must match"
-        assert source_image_points.shape[0] >= 6, "Must have at least 6 points"
+        if order == 2:
+            assert source_image_points.shape[0] >= 6, "Must have at least 6 points"
+        elif order == 1:
+            assert source_image_points.shape[0] >= 3, "Must have at least 3 points"
 
         def create_map(source_image_points, dest_image_points):
             # Create least squared matrix equations for quadratic transformation
             A = []
             B = []
-            for i in range(6):
+            for i in range(order * 3):
                 x_dest, y_dest = dest_image_points[i]
                 x, y = source_image_points[i]
-                A.append([x**2,y**2,x*y,x,y,1])
-                B.append([x_dest, y_dest])
+                if order == 1:
+                    A.append([x,y,1])
+                    B.append([x_dest, y_dest])
+                elif order == 2:
+                    A.append([x**2,y**2,x*y,x,y,1])
+                    B.append([x_dest, y_dest])
             B = np.array(B)
             A = np.array(A, dtype=np.float32)
-            assert B.shape == (6,2), "Shape of matrix B incorrect"
-            assert A.shape == (6,6), "Shape of matrix A incorrect"
+            assert B.shape == (order*3,2), "Shape of matrix B incorrect"
+            assert A.shape == (order*3, order*3), "Shape of matrix A incorrect"
 
             # Solve least squared equation
             M, residuals, rank, s = np.linalg.lstsq(A,B)
@@ -46,7 +55,8 @@ class FitPlane:
     
         return cls(
             M = create_map(source_image_points, dest_image_points),
-            M_rev = create_map(dest_image_points, source_image_points)
+            M_rev = create_map(dest_image_points, source_image_points),
+            order=order
         )
     
     def transform_point(self, source_point, reverse=False):
@@ -60,8 +70,12 @@ class FitPlane:
         else:
             M = self.M
         x,y = source_point
-        x_new = M[0,0]*x**2 + M[1,0]*y**2 + M[2,0]*x*y + M[3,0]*x + M[4,0]*y + M[5,0]
-        y_new = M[0,1]*x**2 + M[1,1]*y**2 + M[2,1]*x*y + M[3,1]*x + M[4,1]*y + M[5,1]
+        if self.order == 2:
+            x_new = M[0,0]*x**2 + M[1,0]*y**2 + M[2,0]*x*y + M[3,0]*x + M[4,0]*y + M[5,0]
+            y_new = M[0,1]*x**2 + M[1,1]*y**2 + M[2,1]*x*y + M[3,1]*x + M[4,1]*y + M[5,1]
+        elif self.order == 1:
+            x_new = M[0,0]*x + M[1,0]*y + M[2,0]
+            y_new = M[0,1]*x + M[1,1]*y + M[2,1]
 
         return np.array([x_new, y_new])
     
