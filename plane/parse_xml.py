39154365138e7db6e27158dad4c754a0020d886f
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2024-09-16 17:20:49 -0700

    cleanup + file delete

--- plane/parse_xml.py
+++ plane/parse_xml.py
@@ -1,29 +1,30 @@
 import xml.etree.ElementTree as ET
 import numpy as np
-import transforms3d
 from scipy.linalg import polar
 
 class ParseXML:
-    def __init__(self, source_points=None, dest_points=None, M=None):
+    def __init__(self, source_points=None, dest_points=None, M=None, z=0):
         self.M = M # Transformation from source image to dest image coordinates
         self.source_points = source_points 
         self.dest_points = dest_points
+        self.z = z
+        self.scale, self.theta_deg, self.shear_magnitude, self.shear_vector, self.tx, self.ty = self._compute_physical(self.M)
 
     @classmethod
-    def extract_data(cls, trakem_filepath, source_patch_num, dest_patch_num, landmarks_filepath=None, multi=False):
+    def from_imagej_xml(cls, trakem_filepath, source_patch_number, dest_patch_number, landmarks_filepath=None, multi=False):
         """
         Function to get transform matrix, source and dest points from XML file of TrakEM2 project.
         Inputs:
             trakem_filepath: path to XML file.
-            source_patch_num: the patch number assigned to your source image by ImageJ.
-            dest_patch_num: patch number assigned to dest image by ImageJ.
+            source_patch_number: the patch number assigned to your source image by ImageJ.
+            dest_patch_number: patch number assigned to dest image by ImageJ.
             landmarks_filepath: the file with exported landmarks from both images.
+        Outputs: returns a ParseXML object describing one pair of photobleach barcodes.
         """
         tree = ET.parse(trakem_filepath)
         root = tree.getroot()
-        # TODO error handling
-        source_patch = root.find(f".//*[@oid='{source_patch_num}']")
-        dest_patch = root.find(f".//*[@oid='{dest_patch_num}']")
+        source_patch = root.find(f".//*[@oid='{source_patch_number}']")
+        dest_patch = root.find(f".//*[@oid='{dest_patch_number}']")
 
         def extract_m(m_string):
             m_string = m_string.replace("matrix(", "").replace(")", "")
@@ -33,11 +34,11 @@ class ParseXML:
         
         source_transform = source_patch.get('transform')
         if not source_transform:
-                raise TypeError(f"No transform found for {source_patch}, patch id {source_patch_num}")
+                raise TypeError(f"No transform found for {source_patch}, patch id {source_patch_number}")
         source_transform = extract_m(source_transform)
         dest_transform = dest_patch.get('transform')
         if not dest_transform:
-                raise TypeError(f"No transform found for {dest_patch}, patch id {dest_patch_num}")
+                raise TypeError(f"No transform found for {dest_patch}, patch id {dest_patch_number}")
         dest_transform = extract_m(dest_transform)
 
         source_transform = np.vstack([source_transform, [0.0, 0.0, 1.0]])
@@ -49,8 +50,8 @@ class ParseXML:
         if landmarks_filepath:
             tree = ET.parse(landmarks_filepath)
             root = tree.getroot()
-            source_points_list = root.findall(f".//*[@patch_id='{source_patch_num}']")
-            dest_points_list = root.findall(f".//*[@patch_id='{dest_patch_num}']")
+            source_points_list = root.findall(f".//*[@patch_id='{source_patch_number}']")
+            dest_points_list = root.findall(f".//*[@patch_id='{dest_patch_number}']")
             for point in source_points_list:
                 source_points.append([float(point.get('x')), float(point.get('y'))])
             for point in dest_points_list:
@@ -71,48 +72,21 @@ class ParseXML:
                 dest_points = dest_points
             )
     
-    # def compute_physical_params_old(self):
-    #     """
-    #     Compute physical representation of transform from matrix M.
-    #     Returns:
-    #         translation (x,y), rotation, scaling x, scaling y, shear.
-    #     """
-    #     M = self.M
-    #     T, R, S, H = transforms3d.affines.decompose(M)
-    #     tx, ty = T
-    #     theta_rad = np.arctan2(R[1, 0], R[0, 0])
-    #     theta_deg = np.degrees(theta_rad)
-    #     sx, sy = S
-    #     shear = H[0]
+    def set_M(self, M):
+        """Set or update M."""
+        self.M = M
+        self.scale, self.theta_deg, self.shear_magnitude, self.shear_vector, self.tx, self.ty = self._compute_physical(self.M)
 
-    #     return tx, ty, theta_deg, sx, sy, shear
-         
-    # def compute_physical_params_svd(self, M):
-    #     # SVD decomposition of a provided matrix
-    #     A = np.array([[M[0,0], M[0,1]], [M[1,0], M[1,1]]])
-    #     U, S, Vt = np.linalg.svd(A)
-    #     print("ssssss:",S)
-    #     s = np.sqrt(S[0] * S[1])  # TODO probably not always true
-    #     # s = np.mean(S)
-    #     R = U @ Vt
-    #     if np.linalg.det(R) < 0:
-    #         Vt[-1, :] *= -1
-    #         R = U @ Vt
-    #     theta_rad = np.arctan2(R[1,0], R[0,0])
-    #     return s, theta_rad
-    
-    def compute_polar(self, M):
+    def _compute_physical(self, M):
         """
         Inputs: 3x3 affine transformation matrix
-        Returns: uniform scaling, rotation angle (deg), volume-preserving shear magnitude and vector
+        Returns: uniform scaling, rotation angle (deg), volume-preserving shear magnitude and vector, translation x, translation y
+        Here, a shear magnitude of 0 represents no shear. Expansion in the direction if > 0, squashing it if < 0
         """
         # Separate out rotation
         M_ul = np.array([[M[0,0], M[0,1]], [M[1,0], M[1,1]]])
         R, S_shear = polar(M_ul)
         theta_rad = np.arctan2(R[1,0], R[0,0])
-        print(np.degrees(theta_rad))
-
-        # --------------------------------------------------------------------------
 
         # Use determinant to find scale
         sc = np.sqrt(np.linalg.det(S_shear))
@@ -122,7 +96,6 @@ class ParseXML:
         ])
         # Isolate shear component
         H = S_shear @ S_inv
-        print(H)
 
         # Find shear magnitude and direction 
         evals, evecs = np.linalg.eig(H)
@@ -132,98 +105,30 @@ class ParseXML:
              shear_magnitude *= -1
              shear_vector *= -1
         shear_vector = shear_vector / np.linalg.norm(shear_vector)
-        
-        return sc, np.degrees(theta_rad), shear_magnitude, shear_vector, M[0,2], M[1,2]
-    
-    # def compute_scale_rotation(self, M):
-    #     M = np.array([[M[0,0], M[0,1]], [M[1,0], M[1,1]]])
-    #     scale = np.sqrt(M[0,0] * M[1,1])
-    #     s = scale * np.eye(2)
-    #     s_inv = np.linalg.inv(s)
-    #     R = M @ s_inv
-    #     theta_rad = np.arctan2(R[0,0], R[1,0])
-    #     return scale, theta_rad
-    
-    # def compute_physical_svd_irreducible(self):
-    #     """
-    #     Compute physical representation of transform from matrix M.
-    #     1) remove average uniform scaling and rotation components using SVD
-    #     2) find shear from remaining matrix using irreducible tensor decomposition
-    #     3) check that rotation and scaling are close to 0
-    #     Returns:
-    #         uniform scale, rotation, shear magnitude, shear vector
-    #     """
-    #     s, theta_rad = self.compute_physical_params_svd(self.M)
 
-    #     S = 1/s * np.eye(2)
-    #     R = np.array([
-    #       [np.cos(theta_rad), -np.sin(theta_rad)],
-    #       [np.sin(theta_rad), np.cos(theta_rad)]])
-    #     print(self.M)
-    #     A = np.array([[self.M[0,0], self.M[0,1]], [self.M[1,0], self.M[1,1]]])
-    #     A = A @ S
-    #     M = A @ np.linalg.inv(R)
-    #     print(M)
-    #     shear = M[0,0]
-
-    #     print("Results:")
-    #     print(s, np.degrees(theta_rad),shear)
-
-    #     # b,c and a,d should be close
-    #     # if not np.all(M == 0):
-    #     #     assert M[0,0] * M[1,1] <= 0, "Cannot find irreducible matrix decomposition, a and d should be opposite signs"
-    #     #     if M[1,0] != 0 and M[0,1] != 0:
-    #     #         assert abs(abs(M[1,0]) - abs(M[0,1])) / ((abs(M[1,0]) + abs(M[0,1])) / 2) < 0.2, "Cannot find irreducible matrix decomposition, b and c not similar"
-    #     #     if M[1,1] != 0 and M[0,0] != 0:
-    #     #         assert abs(abs(M[0,0]) - abs(M[1,1])) / ((abs(M[0,0]) + abs(M[1,1])) / 2) < 0.2, "Cannot find irreducible matrix decomposition, a and d not similar"
-
-    #     # Irreducible tensor decomp of remaining matrix for shear
-    #     # Symmetric part
-    #     M_sym = 0.5 * (M + M.T)
-    #     # Antisymmetric (rotation) part
-    #     M_rotation = 0.5 * (M - M.T)
-    #     # Trace (expansion) part
-    #     trace = np.trace(M)
-    #     M_expansion = (trace / 2) * np.eye(2)
-
-    #     # Symmetric trace-free (shear) part
-    #     M_shear = M_sym - M_expansion
-
-    #     evals, evecs = np.linalg.eig(M_shear)
-    #     shear_magnitude = evals[0]
-    #     shear_vector = evecs[0]
-    #     if shear_magnitude < 0:
-    #          shear_magnitude *= -1
-    #          shear_vector *= -1
-    #     shear_vector = shear_vector / np.linalg.norm(shear_vector)
-        
-    #     # scale and rotation should be close to 0
-    #     # assert M_expansion[0,0] < 0.1
-    #     # assert np.degrees(np.arctan2(M_rotation[0,0], M_rotation[1,0])) % 180 < 10
-
-    #     # Reconstruct and remove shear from original M to find rotation and scaling.
-    #     # TODO temporary?
-    #     # outer_product = np.outer(shear_vector, shear_vector)
-    #     # shear_matrix = np.eye(2) + shear_magnitude * outer_product
-    #     # H_inv = np.linalg.inv(shear_matrix)
-    #     # A_no_shear = A @ H_inv
-    #     # s2, theta2 = self.compute_physical_params_svd(A_no_shear)
-        
-    #     return s, np.degrees(theta_rad), shear_magnitude, shear_vector, self.M[0,2], self.M[1,2]
+        return sc, np.degrees(theta_rad), shear_magnitude-1, shear_vector, M[0,2], M[1,2]
     
     def transform_points(self, points):
-         points = np.array(points)
-         transformed_points = self.M @ points
-         return transformed_points
+        """
+        Inputs: array or list of points (x,y,z)
+        Outputs: np array of transformed points
+        """
+        points = np.array(points)
+        transformed_points = self.M @ points
+        return transformed_points
 
-    def find_transformation_error_from_points(self, source_points, dest_points):
+    def find_transformation_error(self, source_points=None, dest_points=None):
         """
         This function calculates the transformation error between a set of source and destination 
         points. Inputs can be points other than the landmarks already stored in the class object.
         Inputs:
             source_points: an array or list of points (x,y).
             dest_points: must have same shape as source_points.
+        Outputs: average error between transformed source_points and dest_points.
         """
+        if not source_points:
+             source_points = self.source_points
+             dest_points = self.dest_points
         source_points = np.array(source_points)
         dest_points = np.array(dest_points)
         assert source_points.shape == dest_points.shape
@@ -243,43 +148,4 @@ class ParseXML:
         distances = np.linalg.norm(transformed_points-dest_points, axis=1)
         avg_err = np.mean(distances)
         return avg_err
-
-    def find_transformation_error(self):
-        """
-        This function calculates the transformation error between the source and dest points
-        from LANDMARKS.XML. Must be initial landmarks selected BEFORE any alignment was done.
-        Returns:
-            Average distance between respective landmarks after alignment
-        """
-        return self.find_transformation_error_from_points(self.source_points, self.dest_points)
-    
-    def calc_real_scale(self, original_um_per_pixel=1, angle=0):
-        """
-        Function calculates the distance represented per pixel after transformation, along
-        a given direction (useful for calculating the distance between 2 barcodes).
-        Returns: distance represented per pixel along given angle (default 0 = along x axis).
-        Angle increases counterclockwise starting at x axis.
-        """
-        s, theta_deg, shear_magnitude, shear_vector, tx, ty = self.compute_physical_svd_irreducible()
-        # rel_angle = angle # - theta_deg
-        rel_angle = theta_deg
-        rads = np.radians(rel_angle)
-
-        shear_vector = shear_vector/np.linalg.norm(shear_vector)
-        outer_product = np.outer(shear_vector, shear_vector)
-        H = np.eye(2) + shear_magnitude * outer_product
-        det = np.linalg.det(H)
-        H = H / np.sqrt(det)
-
-        S = s * np.eye(2)
-        R = np.array([[np.cos(rads), -np.sin(rads)],
-                    [np.sin(rads), np.cos(rads)]])
-        A = R @ S @ H
-        v = np.array([np.cos(rads), np.sin(rads)])
-        vt = A @ v
-        scaling = np.linalg.norm(vt)
-        print("scaling:",scaling)
-
-        dist_per_pixel = original_um_per_pixel / scaling
-        return dist_per_pixel
     
--- plane/test_parse_xml.py
+++ plane/test_parse_xml.py
@@ -3,8 +3,6 @@ import numpy as np
 import numpy.testing as npt
 import unittest
 from plane.parse_xml import ParseXML
-import matplotlib.pyplot as plt
-import cv2 as cv
 
 class TestParseXML(unittest.TestCase):
 
@@ -12,199 +10,57 @@ class TestParseXML(unittest.TestCase):
       self.tk_filepath = "plane/test_vectors/align-1.xml"
       self.l_filepath = "plane/test_vectors/landmarks.xml"
       
-    # def test_main_function_runs(self):
-    #   ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath)
+    def test_main_function_runs(self):
+      ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, self.l_filepath)
 
-    # def test_parse_data(self):
-    #   # Real saved TrakEM2 XML project files, with original numbers replaced for convenience
-    #   tk_data = ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath)
-    #   self.assertAlmostEqual(tk_data.M[0,0], 1)
-    #   self.assertAlmostEqual(tk_data.M[1,0], 2)
-    #   self.assertAlmostEqual(tk_data.M[0,1], 3)
-    #   self.assertAlmostEqual(tk_data.M[0,2], 500)
-    #   self.assertAlmostEqual(tk_data.M[1,2], 600)
-    #   self.assertAlmostEqual(tk_data.source_points[0,0],800)
-    #   self.assertAlmostEqual(tk_data.source_points[0,1],801)
-    #   self.assertAlmostEqual(tk_data.source_points[1,0],80)
-    #   self.assertAlmostEqual(tk_data.source_points[1,1],81)
-    #   self.assertAlmostEqual(tk_data.dest_points[1,0],11)
-    #   self.assertAlmostEqual(tk_data.dest_points[1,1],10)
+    def test_parse_data(self):
+      # Real saved TrakEM2 XML project files, with original numbers replaced for convenience
+      tk_data = ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, self.l_filepath)
+      self.assertAlmostEqual(tk_data.M[0,0], 1)
+      self.assertAlmostEqual(tk_data.M[1,0], 2)
+      self.assertAlmostEqual(tk_data.M[0,1], 3)
+      self.assertAlmostEqual(tk_data.M[0,2], 500)
+      self.assertAlmostEqual(tk_data.M[1,2], 600)
+      self.assertAlmostEqual(tk_data.source_points[0,0],800)
+      self.assertAlmostEqual(tk_data.source_points[0,1],801)
+      self.assertAlmostEqual(tk_data.source_points[1,0],80)
+      self.assertAlmostEqual(tk_data.source_points[1,1],81)
+      self.assertAlmostEqual(tk_data.dest_points[1,0],11)
+      self.assertAlmostEqual(tk_data.dest_points[1,1],10)
 
-    # def test_find_transformation_error(self):
-    #   tk_data = ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath)
-    #   tk_data.source_points = np.array([
-    #     [1,2],
-    #     [4,5],
-    #     [8,8]
-    #   ])
-    #   # translate y by 2
-    #   tk_data.dest_points = np.array([
-    #     [1,4],
-    #     [6,5],
-    #     [8,10]
-    #   ])
-    #   tk_data.M = np.eye(3)
-    #   err = tk_data.find_transformation_error()
-    #   self.assertAlmostEqual(err, 2)
-
-    # def test_compute_physical_scale_rotate_translate(self):
-    #   # scale then rotate then translate
-    #   # construct transformations
-    #   S = np.array([
-    #       [2, 0, 0],
-    #       [0, 3, 0],
-    #       [0, 0, 1]
-    #   ])
-    #   H = np.array([
-    #       [1, 2, 0],
-    #       [5, 1, 0],
-    #       [0, 0, 1]
-    #   ])
-    #   theta = np.deg2rad(30)
-    #   R = np.array([
-    #       [np.cos(theta), -np.sin(theta), 0],
-    #       [np.sin(theta), np.cos(theta), 0],
-    #       [0, 0, 1]
-    #   ])
-
-    #   T = np.array([
-    #       [1, 0, 10],
-    #       [0, 1, 20],
-    #       [0, 0, 1]
-    #   ])
-
-    #   M = T @ R @ S @ H
-    #   tk_data = ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath)
-    #   tk_data.M = M
-    #   tx, ty, theta, sx, sy, shear = tk_data.compute_physical_params_old()
-
-    #   S2 = np.array([
-    #       [sx, 0, 0],
-    #       [0, sy, 0],
-    #       [0, 0, 1]
-    #   ])
-    #   H2 = np.array([
-    #       [1, shear, 0],
-    #       [0, 1, 0],
-    #       [0, 0, 1]
-    #   ])
-    #   theta = np.deg2rad(theta)
-    #   R2 = np.array([
-    #       [np.cos(theta), -np.sin(theta), 0],
-    #       [np.sin(theta), np.cos(theta), 0],
-    #       [0, 0, 1]
-    #   ])
-
-    #   T2 = np.array([
-    #       [1, 0, tx],
-    #       [0, 1, ty],
-    #       [0, 0, 1]
-    #   ])
-    #   M2 = T2 @ R2 @ S2 @ H2
-      
-    #   for row in range(0, M2.shape[0]):
-    #      for col in range(0, M2.shape[1]):
-    #         self.assertAlmostEqual(M2[row,col], M[row, col])
-
-    # def test_compute_physical_from_xml(self):
-    #   # scale then rotate then translate
-    #   tk_data = ParseXML.extract_data("plane/test_vectors/trakem-sample.xml", 8, 11, "plane/test_vectors/landmarks-sample.xml")
-    #   tx, ty, theta, sx, sy, shear = tk_data.compute_physical_params_old()
-
-    #   S2 = np.array([
-    #       [sx, 0, 0],
-    #       [0, sy, 0],
-    #       [0, 0, 1]
-    #   ])
-    #   H2 = np.array([
-    #       [1, shear, 0],
-    #       [0, 1, 0],
-    #       [0, 0, 1]
-    #   ])
-    #   theta = np.deg2rad(theta)
-    #   R2 = np.array([
-    #       [np.cos(theta), -np.sin(theta), 0],
-    #       [np.sin(theta), np.cos(theta), 0],
-    #       [0, 0, 1]
-    #   ])
-
-    #   T2 = np.array([
-    #       [1, 0, tx],
-    #       [0, 1, ty],
-    #       [0, 0, 1]
-    #   ])
-    #   M2 = T2 @ R2 @ S2 @ H2
-      
-    #   for row in range(0, M2.shape[0]):
-    #      for col in range(0, M2.shape[1]):
-    #         self.assertAlmostEqual(M2[row,col], tk_data.M[row, col])
+    def test_find_transformation_error(self):
+      tk_data = ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, self.l_filepath)
+      tk_data.set_M(np.eye(3))
+      tk_data.source_points = np.array([
+        [1,2],
+        [4,5],
+        [8,8]
+      ])
+      # translate y by 2
+      tk_data.dest_points = np.array([
+        [1,4],
+        [6,5],
+        [8,10]
+      ])
+      err = tk_data.find_transformation_error()
+      self.assertAlmostEqual(err, 2)
     
-    # def test_compute_error(self):
-    #   test_project = ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath, True)
-    #   test_project.M = np.array([[1,0,0], [0,1,0], [0,0,1]])
-    #   test_project.source_points = np.array([[1,2], [3,4]])
-    #   test_project.dest_points = np.array([[3, 2], [3,6]])
-    #   err = test_project.find_transformation_error()
-    #   self.assertAlmostEqual(err, 2)
+    def test_compute_error(self):
+      test_project = ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, self.l_filepath, True)
+      test_project.set_M(np.eye(3))
+      test_project.source_points = np.array([[1,2], [3,4]])
+      test_project.dest_points = np.array([[3, 2], [3,6]])
+      err = test_project.find_transformation_error()
+      self.assertAlmostEqual(err, 2)
 
-    #   test_project.M = np.array([[1,0,2], [0,1,5], [0,0,1]])
-    #   test_project.source_points = np.array([[1,2], [3,4]])
-    #   test_project.dest_points = np.array([[3, 7], [5,9]])
-    #   err = test_project.find_transformation_error()
-    #   self.assertAlmostEqual(err, 0)
-
-    # def test_calc_new_scale(self):
-    #   test_project = ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath, True)
-    #   test_project.M = np.array([[1,0,0], [0,1,0], [0,0,1]])
-    #   self.assertAlmostEqual(test_project.calc_real_scale(1, 0), 1)
-
-    #   test_project.M = np.array([[2,0,0], [0,1,0], [0,0,1]])
-    #   self.assertAlmostEqual(test_project.calc_real_scale(1, 0), 0.5)
-    #   self.assertAlmostEqual(test_project.calc_real_scale(2, 90), 2/3)
-
-    # def test_physical_svd(self):
-    #   test_project = ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath, True)
-    #   test_project.M = np.array([[2,0,0], [0,2,0], [0,0,1]])
-    #   theta = np.deg2rad(30)
-    #   R2 = np.array([
-    #       [np.cos(theta), -np.sin(theta), 0],
-    #       [np.sin(theta), np.cos(theta), 0],
-    #       [0, 0, 1]
-    #   ])
-    #   test_project.M = test_project.M @ R2
-    #   s, theta = test_project.compute_physical_params_svd(test_project.M)
-    #   self.assertAlmostEqual(s, 2)
-    #   self.assertAlmostEqual(np.degrees(theta), 30)
-      
-    # def test_physical_svd_irreducible(self):
-    #   test_project = ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath, True)
-    #   test_project.M = np.array([[1,0,0], [0,1,0], [0,0,1]])
-    #   theta = np.deg2rad(32)
-    #   R2 = np.array([
-    #       [np.cos(theta), -np.sin(theta), 0],
-    #       [np.sin(theta), np.cos(theta), 0],
-    #       [0, 0, 1]
-    #   ])
-    #   H = np.array([
-    #       [1, 0.03, 0],
-    #       [0.03, 1, 0],
-    #       [0, 0, 1]
-    #   ])
-    #   S = np.array([
-    #       [1.3, 0, 0],
-    #       [0, 1.3, 0],
-    #       [0, 0, 1]
-    #   ])
-    #   test_project.M = test_project.M @ R2 @ S @ H
-    #   s, theta, mag, dir, tx, ty = test_project.compute_physical_svd_irreducible()
-    #   self.assertAlmostEqual(s, 1.3)
-    #   self.assertAlmostEqual(theta, 32)
+      test_project = ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, self.l_filepath, True)
+      test_project.set_M(np.array([[1,0,2], [0,1,5], [0,0,1]]))
+      test_project.source_points = np.array([[1,2], [3,4]])
+      test_project.dest_points = np.array([[3, 7], [5,9]])
+      err = test_project.find_transformation_error()
+      self.assertAlmostEqual(err, 0)
     
     def test_physical_POLAR(self):
-
-      # yay!
-      test_project = ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath, True)
-
       deg = 10
       theta = np.deg2rad(deg)
       R2 = np.array([
@@ -224,23 +80,24 @@ class TestParseXML(unittest.TestCase):
          [0,0,1]
       ])
 
-      test_project.M = np.array([
-          [1, 0, 0],
-          [0, 1, 0],
-          [0, 0, 1]
-      ])
-      test_project.M = test_project.M @ R2 @ H @ S
-      s, r, m, v ,tx, ty = test_project.compute_polar(test_project.M)
+      test_project = ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, self.l_filepath, True)
+      test_project.set_M(np.eye(3) @ R2 @ H @ S)
+      s = test_project.scale
+      r = test_project.theta_deg
+      m = test_project.shear_magnitude
+      v = test_project.shear_vector
       self.assertAlmostEqual(r, deg)
       self.assertAlmostEqual(s, scale)
-      self.assertAlmostEqual(m, 2)
+      self.assertAlmostEqual(m, 1)
       self.assertAlmostEqual(v[0], 1)
     
     def test_polar_real_matrix(self):
-      test_project = ParseXML.extract_data(self.tk_filepath, 8, 11, self.l_filepath, True)
-      test_project.M = np.array([[ 1.84403066, -2.40635941e-01 , 4.98432352e+02],[ 2.56386722e-01 , 2.04612135e+00 , 2.72037605e+03],[ 0.00000000e+00 , 0.00000000e+00,  1.00000000e+00]])
-      s, r, m, v ,tx, ty = test_project.compute_polar(test_project.M)
-      print(s,r,m,v)
+      test_project = ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, self.l_filepath, True)
+      test_project.set_M(np.array([[ 1.84403066, -2.40635941e-01 , 4.98432352e+02],[ 2.56386722e-01 , 2.04612135e+00 , 2.72037605e+03],[ 0.00000000e+00 , 0.00000000e+00,  1.00000000e+00]]))
+      s = test_project.scale
+      r = test_project.theta_deg
+      m = test_project.shear_magnitude
+      v = test_project.shear_vector
 
 if __name__ == '__main__':
   unittest.main()
