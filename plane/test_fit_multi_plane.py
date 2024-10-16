297172bead9811ca337ea02e0fd79efd275d9073
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2024-10-15 22:14:01 -0700

    create tester file

---
+++ plane/test_fit_multi_plane.py
@@ -0,0 +1,13 @@
+import math
+import numpy as np
+import numpy.testing as npt
+import unittest
+from plane.fit_multi_plane import FitMultiPlane
+
+class TestFitMultiPlane(unittest.TestCase):
+
+    def setUp(self):
+      pass
+
+    def test_main_function_runs(self):
+       pass
