c38f572af0a7e50c5c470b3c013c93670df0a438
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2024-10-16 19:27:35 -0700

    calc_distance function + tester

--- plane/fit_multi_plane.py
+++ plane/fit_multi_plane.py
@@ -1,8 +1,8 @@
 import numpy as np
-from parse_xml import ParseXML
+from plane.parse_xml import ParseXML
 
 class FitMultiPlane:
-    def __init__(self):
+    def __init__(self, landmarks, real_centers):
         """
         - list of barcodes contained in object
             - contains self.scale, self.theta_deg, self.shear_magnitude, self.shear_vector, self.tx, self.ty, self.z
@@ -10,21 +10,39 @@ class FitMultiPlane:
         - Adjacency matrix to store distances between pairs of barcodes.
         - ??? to store angles between 3 barcodes for xy, xz, yz planes 
         """
-        pass
+        self.landmarks = landmarks
+        self.real_centers = real_centers
+        self.distances = self.calc_distances()
+        self.angles = self.calc_angles()
+
 
     @classmethod
-    def from_imagej_xml(cls):
+    def from_aligned_landmarks(cls, landmarks, real_centers):
         """
         Function to calculate/store the params for individual barcodes and combinations of barcodes. 
             - Individual barcodes: xy center position, rotation angle on xy plane, shear, scaling
             - Distances and angles between barcodes
+
+        Inputs:
+            - landmarks: array-like of fitted planes (ParseXML objects)
+            - real_centers: array-like with the locations of the landmarks as defined by photobleach script
         """
-        pass
+        return cls(landmarks, real_centers)
 
     def calc_distances(self):
         """
         Set adjacency matrix for the list of barcodes given, using their tx ty params.
         """
+        n = len(self.landmarks)
+        adj = np.zeros((n,n))
+        for i in range(0, n):
+            for j in range(0,n):
+                dist = np.sqrt((self.landmarks[i].tx - self.landmarks[j].tx)**2 + (self.landmarks[i].ty - self.landmarks[j].ty)**2)
+                adj[i,j] = dist
+        return adj
+
+
+    def calc_angles(self):
         pass
     
     def calc_angles_xy(self):
--- plane/test_fit_multi_plane.py
+++ plane/test_fit_multi_plane.py
@@ -3,11 +3,34 @@ import numpy as np
 import numpy.testing as npt
 import unittest
 from plane.fit_multi_plane import FitMultiPlane
+from plane.parse_xml import ParseXML
 
 class TestFitMultiPlane(unittest.TestCase):
 
     def setUp(self):
-      pass
+        self.tk_filepath = "plane/test_vectors/align-1.xml"
 
-    def test_main_function_runs(self):
-       pass
+    def test_main_runs(self):
+        landmarks = []
+        real_centers = []
+        FitMultiPlane.from_aligned_landmarks(landmarks, real_centers)
+
+    def test_calc_dists(self):
+        fp1 = ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, None)
+        fp2 = ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, None)
+        fp3 = ParseXML.from_imagej_xml(self.tk_filepath, 8, 11, None)
+
+        fp3.tx = 10
+        fp3.ty = 10
+        fp2.tx = 3
+        fp2.ty = 4
+        fp1.tx = 0
+        fp1.ty = 0
+
+        landmarks = [fp1, fp2, fp3]
+        real_centers = [(0, 50), (50, 100), (0,0)]
+        mp = FitMultiPlane.from_aligned_landmarks(landmarks, real_centers)
+
+        adj = mp.calc_distances()
+        assert adj[1,0] == adj[0,1]
+        self.assertAlmostEqual(adj[1,0], 5)
