0ce38cbb1978f4b6108bee83c9f9756b39ab1e26
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2024-10-31 10:12:00 -0700

    FMP constructor, test data, tester

--- plane/fit_multi_plane.py
+++ plane/fit_multi_plane.py
@@ -1,5 +1,6 @@
 import numpy as np
-from plane.fit_plane import FitPlane
+# from plane.fit_plane import FitPlane
+from plane.parse_xml import ParseXML
 import pandas as pd
 
 class FitMultiPlane:
@@ -41,35 +42,13 @@ class FitMultiPlane:
         Set adjacency matrix for the list of barcodes given, using their tx ty params.
         Units are in um.
         """
-        ## in pixels:
-        # n = len(self.fitplanes)
-        # adj = np.zeros((n,n))
-        # for i in range(0, n):
-        #     for j in range(0,n):
-        #         dist = np.sqrt((self.fitplanes[i].tx - self.fitplanes[j].tx)**2 + (self.fitplanes[i].ty - self.fitplanes[j].ty)**2)
-        #         adj[i,j] = dist
-        # return adj
-    
-        # in um
-        n = len(self.fitplanes)
-        adj = np.zeros((n,n))
-        for i in range(0, n):
-            for j in range(0,n):
-                dist = np.sqrt((self.fitplane_centers[i, 0] - self.fitplanes[j,0])**2 + (self.fitplanes[i, 1] - self.fitplanes[j, 1])**2)
-                adj[i,j] = dist
-        return adj
+        pass
 
     def calc_fitplane_centers(self):
         """
-        Convert Fitplane centers to distance in um, and add z coordinate for each.
+        Convert Fitplane centers in pixels to distance in um, and add z coordinate for each.
         """
-        centers  = [(project.tx + self.template_size/2, project.ty + self.template_size/2) for project in self.fitplanes]
-        centers = np.array(centers)
-        avgscale = np.mean([project.scale for project in ps])
-        centers = centers * (self.um_per_pixel / avgscale) # convert fluorescent units from pixels to um
-        zs = np.array([project.z for project in self.fitplanes])
-        centers_z = np.column_stack((centers, zs))
-        return centers_z
+        pass
 
     def fit_mapping_to_xy(self):
         """
@@ -77,78 +56,14 @@ class FitMultiPlane:
         UVH mapping: for a point (u,v,z') on the sliced tissue, [x,y,z] = vec_u * u + vec_v * v + vec_h
         Prints and returns vectors U, V, and H.
         """
-        u = np.array([x[0] for x in self.fitplane_centers])
-        v = np.array([x[1] for x in self.fitplane_centers])
-
-        x = np.array([x[0] for x in self.real_centers])
-        y = np.array([x[1] for x in self.real_centers])
-        z = np.ones_like(y)
-
-        # Number of points
-        n = u.shape[0]
-
-        A = np.zeros((3 * n, 9))
-        for i in range(n):
-            A[3 * i] = [u[i], v[i], 1, 0, 0, 0, 0, 0, 0]      # x equation
-            A[3 * i + 1] = [0, 0, 0, u[i], v[i], 1, 0, 0, 0]  # y equation
-            A[3 * i + 2] = [0, 0, 0, 0, 0, 0, u[i], v[i], 1]  # z equation
-
-        # Output vector b
-        b = np.zeros(3 * n)
-        for i in range(n):
-            b[3 * i] = x[i]
-            b[3 * i + 1] = y[i]
-            b[3 * i + 2] = z[i]
-
-        # Solve using least squares
-        M, residuals, rank, s = np.linalg.lstsq(A, b, rcond=None) # TODO check that lol
-
-        ux, vx, hx, uy, vy, hy, uz, vz, hz = M
-
-        print("Transformation coefficients:")
-        print("Vector u:", [ux, uy, uz])
-        print("Vector v:", [vx, vy, vz])
-        print("Vector h:", [hx, hy, hz])
-
-        return np.array([ux, uy, uz]), np.array([vx, vy, vz]), np.array([hx, hy, hz])
+        pass
 
     def print_single_plane_stats(self):
         """
         Prints stats for each FitPlane as a table: shrinkage, rotation, shear, and mean/stdev for each
         Units: um
         """
-        num_templates = len(self)
-        projects_data = {
-        "Template ID": [i for i in range(1, num_templates+1)],
-        "Patch Number": [project.template_id for project in self.fitplanes],
-        "Z (um)": [project.z for project in self.fitplanes],
-        "Center (x)": [project.tx + self.template_size/2 for project in self.fitplanes],
-        "Center (y)": [project.ty + self.template_size/2 for project in self.fitplanes],
-        "Rotation (deg)": [project.theta_deg for project in self.fitplanes],
-        "Scaling": [project.scale for project in self.fitplanes],
-        "Shear magnitude": [project.shear_magnitude for project in self.fitplanes],
-        "Shear vector (x)": [project.shear_vector[0] for project in self.fitplanes],
-        "Shear vector (y)": [project.shear_vector[1] for project in self.fitplanes]
-        }
-
-        columns_to_summarize = ["Z (um)", "Rotation (deg)", "Scaling", "Shear magnitude", "Shear vector (x)", "Shear vector (y)"]
-
-        # Create DataFrame
-        df = pd.DataFrame(projects_data)
-
-        # Compute mean and standard deviation for selected columns only
-        mean_row = df[columns_to_summarize].mean()
-        std_row = df[columns_to_summarize].std()
-
-        # Append mean and std as new rows for selected columns only
-        summary_df = df.copy()
-        summary_df.loc['Mean', columns_to_summarize] = mean_row
-        summary_df.loc['StDev', columns_to_summarize] = std_row
-        summary_df = summary_df.round(2)
-        summary_df = summary_df.replace(np.nan, '', regex=True)
-
-        print("Stats for individual barcodes:\n")
-        print(summary_df)
+        pass
 
     def project_centers_onto_flat_plane(self):
         """
@@ -156,21 +71,11 @@ class FitMultiPlane:
         vectors UVH from fit_mapping_to_xy. Z for the flat plane is 0.
         Returns: array-like of transformed points.
         """
-        u_coords = self.fitplane_centers[:, 0]
-        v_coords = self.fitplane_centers[:, 1]
-        
-        # Project each (u, v) pair using the vectors u, v, h
-        u,v,h = self.fit_mapping_to_xy()
-        projected = np.outer(u_coords, u) + np.outer(v_coords, v) + h
-        return projected
+        pass
     
     def compute_avg_projection_error(a, b):
         """
         Returns the mean distance between points in arrays a and b, for evaluating best-fit calculated projection.
         Ignores z coordinate.
         """
-        assert a.shape == b.shape, "Input arrays must have the same shape"
-        if a.shape[1] == 3:
-            a = a[:, :2]
-            b = b[:, :2]
-        return np.mean(np.linalg.norm(a - b, axis=1))
+        pass
--- plane/test_fit_multi_plane.py
+++ plane/test_fit_multi_plane.py
@@ -3,11 +3,17 @@ import numpy as np
 import numpy.testing as npt
 import unittest
 from plane.fit_multi_plane import FitMultiPlane
+from plane.parse_xml import ParseXML
 
 class TestFitMultiPlane(unittest.TestCase):
 
     def setUp(self):
-      pass
+      tk_filepath = "plane/test_vectors/align3.xml"
+      fp1 = ParseXML.from_imagej_xml(tk_filepath, 8, 11, None)
+      fp2 = ParseXML.from_imagej_xml(tk_filepath, 8, 14, None)
+      fp3 = ParseXML.from_imagej_xml(tk_filepath, 8, 17, None)
+      self.fp_list = [fp1, fp2, fp3]
+      self.real_centers_list = [[0,1000], [1000, 1000], [0, 0]]
 
     def test_main_function_runs(self):
-       pass
+       FitMultiPlane.from_aligned_fitplanes(self.fp_list, self.real_centers_list, template_size=401, um_per_pixel=2)
