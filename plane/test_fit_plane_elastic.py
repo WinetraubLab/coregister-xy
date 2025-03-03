0efce4a8dfcb98f1e1a12d6c9b826b6537cf10b5
zerlinazl <76027329+zerlinazl@users.noreply.github.com>
2025-03-02 16:35:34 -0800

    Add test case

--- plane/test_fit_plane_elastic.py
+++ plane/test_fit_plane_elastic.py
@@ -9,19 +9,26 @@ class TestFitPlaneElastic(unittest.TestCase):
 
     def setUp(self):
         self.fluorescent_image_points_positions_uv_pix = [[0, 1], [1, 0], [1, 1], [0.5, 0.5]]
-        self.template_center_positions_xyz_mm = [[0, 1, 0], [1, 0, 0], [1, 1, 0], [0.5, 0.5, 0]]
+        self.template_positions_xyz_mm = [[0, 1, 0], [1, 0, 0], [1, 1, 0], [0.5, 0.5, 0]]
 
     def test_main_function_runs(self):
-        FitPlaneElastic.from_points(self.fluorescent_image_points_positions_uv_pix, self.template_center_positions_xyz_mm, print_inputs=False)
-        FitPlaneElastic.from_points(self.fluorescent_image_points_positions_uv_pix, self.template_center_positions_xyz_mm, print_inputs=True)
+        FitPlaneElastic.from_points(self.fluorescent_image_points_positions_uv_pix, self.template_positions_xyz_mm, print_inputs=False)
+        FitPlaneElastic.from_points(self.fluorescent_image_points_positions_uv_pix, self.template_positions_xyz_mm, print_inputs=True)
     
     def test_get_xyz_from_uv(self):
-        fp = FitPlaneElastic.from_points(self.fluorescent_image_points_positions_uv_pix, self.template_center_positions_xyz_mm, print_inputs=False)
+        fp = FitPlaneElastic.from_points(self.fluorescent_image_points_positions_uv_pix, self.template_positions_xyz_mm, print_inputs=False)
         xyz = fp.get_xyz_from_uv(self.fluorescent_image_points_positions_uv_pix)
-        npt.assert_array_almost_equal(xyz, self.template_center_positions_xyz_mm)
+        # Check that transformation projects uv to xyz 
+        npt.assert_array_almost_equal(xyz, self.template_positions_xyz_mm)
 
     def test_get_uv_from_xyz(self):
-        uv_pix = [[4, 1], [5, 0], [5, 1], [4.5, 0.5]]
-        fp = FitPlaneElastic.from_points(uv_pix, self.template_center_positions_xyz_mm, print_inputs=False)
-        uv = fp.get_uv_from_xyz(self.template_center_positions_xyz_mm)
-        npt.assert_array_almost_equal(uv, uv_pix)
+        fp = FitPlaneElastic.from_points(self.fluorescent_image_points_positions_uv_pix, self.template_positions_xyz_mm, print_inputs=False)
+        uv = fp.get_uv_from_xyz(self.template_positions_xyz_mm)
+        # Check that transformation projects xyz to uv
+        npt.assert_array_almost_equal(uv, self.fluorescent_image_points_positions_uv_pix)
+
+    def test_uv_to_xyz_back_to_uv(self):
+        fp = FitPlaneElastic.from_points(self.fluorescent_image_points_positions_uv_pix, self.template_positions_xyz_mm, print_inputs=False)
+        xyz = fp.get_xyz_from_uv(self.fluorescent_image_points_positions_uv_pix)
+        uv = fp.get_uv_from_xyz(xyz)
+        npt.assert_array_almost_equal(uv, self.fluorescent_image_points_positions_uv_pix)
